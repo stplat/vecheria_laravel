d0e357721aefea22d9c60c72e7e9124f1a3ef3fd
stsp <st.platonov@gmail.com>
2019-12-31 15:18:25 +0300

    Редиректы (дом)

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -8,44 +8,25 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class HttpsProtocol extends Middleware {
-
+  
   public function handle($request, Closure $next) {
-    /*if (!$request->secure() && App::environment() == 'production') {
-      return redirect()->secure($request->getRequestUri());
-    }
-
-    if(config('app.env') == 'production'){
-
-      $host = $request->header('host');
-      if (substr($host, 0, 4) != 'www.') {
-        if(!$request->secure()){
-          $request->server->set('HTTPS', true);
-        }
-        $request->headers->set('host', 'www.'.$host);
-        return Redirect::to($request->path(),301);
-      }else{
-        if(!$request->secure()){
-          $request->server->set('HTTPS', true);
-          return Redirect::to($request->path(),301);
-        }
-      }
-    }*/
-
-    /*if (App::environment() === 'production') {
-
+    if (App::environment() === 'production') {
+      
       $host = $request->header('host');
       if (substr($host, 0, 4) !== 'www.') {
         if (!$request->secure()) {
-          return redirect()->secure($request->getRequestUri());
+          $request->server->set('HTTPS', true);
+          return redirect()->to($request->path(), 301);
         }
       } else {
         if (!$request->secure()) {
-          $request->headers->set('host', 'vecheria.ru');
-          return redirect()->secure($request->getRequestUri());
+          $request->server->set('HTTPS', true);
         }
+        $request->headers->set('host', 'vecheria.ru');
+        return redirect()->to($request->path(), 301);
       }
-    }*/
-
+    }
+    
     return $next($request);
   }
 }
